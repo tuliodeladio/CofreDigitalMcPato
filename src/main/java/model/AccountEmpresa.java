3113ff880c9956809a671ba5af6c18c0cead2839
Matheus Porto <matheus.pereira11@gmail.com>
2026-04-08 21:29:41 -0300

    refactor(account-empresa): add document number

--- src/main/java/model/AccountEmpresa.java
+++ src/main/java/model/AccountEmpresa.java
@@ -5,7 +5,7 @@ public class AccountEmpresa extends Account {
 
     public AccountEmpresa(String accountNumber, String name, String email,
                           String passwordHash, String cnpj) {
-        super(accountNumber, name, email, passwordHash);
+        super(accountNumber, name, email, passwordHash, cnpj);
         this.cnpj = cnpj;
     }
 
