48178fced4ede385831c0dc6853fd788b7e8a551
Matheus Porto <matheus.pereira11@gmail.com>
2026-04-08 21:07:09 -0300

    test(account): test coverage for findByEmail

--- src/main/java/tests/TesteDB.java
+++ src/main/java/tests/TesteDB.java
@@ -1,6 +1,7 @@
 package tests;
 
 import dao.AccountDAO;
+import model.Account;
 import model.AccountPessoaFisica;
 
 public class TesteDB {
@@ -31,6 +32,11 @@ public class TesteDB {
         dao.listAll().forEach(a ->
                 System.out.println(a.getAccountNumber() + " - " + a.getName())
         );
+
+        // Find By Email
+        System.out.println("\nBuscando conta por email teste@email.com");
+        Account account = AccountDAO.findByEmail("teste@email.com");
+        System.out.println(account.getAccountNumber() + " - " + account.getName() + " - " + account.getEmail());
 
         // DELETE
         dao.delete("9999999999");
