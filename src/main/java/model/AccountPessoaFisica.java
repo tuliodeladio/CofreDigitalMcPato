d6384154d3e6103a5769a13c96d9ef967624f8c8
Matheus Porto <matheus.pereira11@gmail.com>
2026-04-08 21:29:52 -0300

    refactor(account-pessoa-fisica): add document number

--- src/main/java/model/AccountPessoaFisica.java
+++ src/main/java/model/AccountPessoaFisica.java
@@ -5,7 +5,7 @@ public class AccountPessoaFisica extends Account {
 
     public AccountPessoaFisica(String accountNumber, String name, String email,
                                String passwordHash, String cpf) {
-        super(accountNumber, name, email, passwordHash);
+        super(accountNumber, name, email, passwordHash, cpf);
         this.cpf = cpf;
     }
 
