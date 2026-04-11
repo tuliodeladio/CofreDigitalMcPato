12e8a4a31c4ad8b685d7610c115314ccb638b918
Matheus Porto <matheus.pereira11@gmail.com>
2026-04-11 16:05:09 -0300

    build: code cleanup

--- src/main/java/factory/ConnectionFactory.java
+++ src/main/java/factory/ConnectionFactory.java
@@ -4,8 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionFactory {
-
-    // URL FIAP: jdbc:oracle:thin:@//oracle.fiap.com.br:1521/ORCL
     private static final String URL = System.getenv("DB_URL");
     private static final String USER = System.getenv("DB_USER");
     private static final String PASSWORD = System.getenv("DB_PASS");
