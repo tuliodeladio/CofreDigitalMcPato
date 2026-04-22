83a835d4c371411afb95f447f3ef61d6d766559c
Matheus Porto <matheus.pereira11@gmail.com>
2026-04-22 18:41:38 -0300

    refactor(validators/transfer): update validator to use an account asset object

--- src/main/java/validator/TransferValidator.java
+++ src/main/java/validator/TransferValidator.java
@@ -3,12 +3,12 @@ package validator;
 import model.*;
 
 public class TransferValidator {
-    public static void validateTransfer(Account from, Account to, String symbol, double amount) {
-        if (from == null || to == null || symbol == null || amount <= 0)
+    public static void validateTransfer(Account from, Account to, AccountAsset asset, double amount) {
+        if (from == null || to == null || asset == null || amount <= 0)
             throw new ValidationException("Dados inválidos para transferência!");
         if (from.getAccountNumber().equals(to.getAccountNumber()))
             throw new ValidationException("Conta de origem e destino são iguais!");
-        if (from.getAsset(symbol) < amount)
+        if (asset.getQuantity() < amount)
             throw new ValidationException("Saldo insuficiente no ativo para transferir!");
     }
 }
