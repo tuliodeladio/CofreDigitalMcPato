30e50f9e0c2a0caa00c88b75befff319b62918bf
Matheus Porto <matheus.pereira11@gmail.com>
2026-04-22 19:59:09 -0300

    refactor(transfer): fixes on service

--- src/main/java/service/TransferService.java
+++ src/main/java/service/TransferService.java
@@ -54,8 +54,8 @@ public class TransferService {
             return;
         }
 
-        contaOrigem.removeAsset(asset, quantity);
-        contaDestino.addAsset(asset, quantity);
+        contaOrigem.removeAsset(assetSymbol, quantity);
+        contaDestino.addAsset(assetSymbol, quantity);
 
         adicionarTransferencia(
             contaOrigem.getAccountNumber(),
