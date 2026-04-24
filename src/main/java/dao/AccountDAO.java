c7ebc08f3d77f9032abba1d01cf7064a8110502d
Matheus Porto <matheus.pereira11@gmail.com>
2026-04-24 17:19:18 -0300

    refactor(account-dao): improvements on imports

--- src/main/java/dao/AccountDAO.java
+++ src/main/java/dao/AccountDAO.java
@@ -5,7 +5,10 @@ import model.Account;
 import model.AccountPessoaFisica;
 import model.AccountEmpresa;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
